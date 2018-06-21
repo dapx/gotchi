5141e1fdf5660651706e72c4b453785e0b3cd6f3
Daniel André da Silva <dap1995@gmail.com>
2018-06-20 22:45:57 -0300

    Move App to source directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
 
 import React, { Component } from 'react';
@@ -19,8 +18,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
