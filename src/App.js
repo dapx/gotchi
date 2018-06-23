a924278b1a624f60a8822a778fcf960fbb090c6f
Daniel André da Silva <dap1995@gmail.com>
2018-06-23 11:19:44 -0300

    Create a character page

--- src/App.js
+++ src/App.js
@@ -7,67 +7,14 @@ import * as React from 'react';
 import {
   StyleSheet,
   View,
-  Text,
 } from 'react-native';
-import { CharHeader } from './components/CharHeader';
-import SkillBar from './components/SkillBar';
-
-const character = {
-  name: 'Orclord',
-  image: 'https://opengameart.org/sites/default/files/Orc_ani_run008.png',
-  strength: 0.9,
-  defense: 0.7,
-  agility: 0.2,
-  intelligence: 0.4,
-}
+import Character from './scenes/Character';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <CharHeader
-          name={character.name}
-          image={character.image}
-        />
-        <View style={styles.skillsContainer}>
-          <View style={styles.skillsTitle}>
-            <Text style={styles.skillsTitleText}>
-              {'Skills'}
-            </Text>
-          </View>
-          <View style={styles.skillsContentBox}>
-            <View style={styles.skillsContent}>
-              <View style={styles.skill}>
-                <SkillBar
-                  label="Strength"
-                  value={character.strength}
-                  color="#e53935"
-                />
-              </View>
-              <View style={styles.skill}>
-                <SkillBar
-                  label="Defense"
-                  value={character.defense}
-                  color="#1E88E5"
-                />
-              </View>
-              <View style={styles.skill}>
-                <SkillBar
-                  label="Agility"
-                  value={character.agility}
-                  color="#FDD835"
-                />
-              </View>
-              <View style={styles.skill}>
-                <SkillBar
-                  label="Intelligence"
-                  value={character.intelligence}
-                  color="#FB8C00"
-                />
-              </View>
-            </View>
-          </View>
-        </View>
+        <Character />
       </View>
     );
   }
@@ -76,41 +23,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  skillsContainer: {
-    flex: 2,
-    margin: 10,
-    borderWidth: 2,
-    borderColor: '#ddd',
-  },
-  skillsTitle: {
-    flex: 1,
-    borderBottomWidth: 1,
-    borderColor: '#ddd',
-  },
-  skillsTitleText: {
-    flex: 1,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    padding: 10,
-  },
-  skillsContentBox: { flex: 10 },
-  skillsContent: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    margin: 20,
-    borderColor: '#ddd',
-    justifyContent: 'space-around',
-  },
-  skill: {
-    margin: 10,
-    minWidth: 100,
-    borderColor: '#ddd',
-    alignItems: 'flex-start',
-  },
-  skillName: {
-    color: '#ddd'
   },
 });
