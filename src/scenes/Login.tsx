8c739c7ff2c3c4ebec90a2007ba4a0ab221aac11
Daniel André da Silva <dap1995@gmail.com>
2018-07-03 22:07:49 -0300

    Implement Login scene handlers

--- src/scenes/Login.tsx
+++ src/scenes/Login.tsx
@@ -6,10 +6,61 @@ import {
   Image,
 } from 'react-native';
 import { Form, Item, Label, Input, Button } from 'native-base';
+import Api from '../utils/api';
+
 const logo = require('../../temporary.png');
 
-class Login extends React.PureComponent {
+interface LoginResponse {
+  user: string,
+  jwt: string,
+  name: string,
+}
+
+// TODO - Search how to type navigation object properties.
+interface Navigation {
+  navigate: any,
+}
+
+interface Props {
+  navigation: Navigation,
+}
+
+interface State {
+  username: string,
+  password: string,
+  loading: boolean,
+}
+
+class Login extends React.PureComponent<Props, State> {
+  state = {
+    username: '',
+    password: '',
+    loading: false
+  }
+
+  handleUsername = (username: string) => this.setState({ username })
+  handlePassword = (password: string) => this.setState({ password })
+
+  onLogin = () => {
+    this.setState({ loading: true });
+    /**
+     * Just a test, it is not the real request.
+     * To work right:
+     * Api.insecure_request('auth/signin', { ...userData }, 'POST').then(... => goToHome);
+     */
+    Api.insecure_request('').then((data: LoginResponse) => {
+      this.setState({ loading: false });
+      this.props.navigation.navigate('Home');
+    }).catch(error => {
+      // Call Toast to show message to the user.
+      // Shows the error message
+      this.setState({ loading: false });
+      console.error(error);
+    });
+  }
+
   render() {
+    const { username, password } = this.state;
     return (
       <View style={styles.container}>
         <Form style={styles.loginContainer}>
@@ -18,20 +69,28 @@ class Login extends React.PureComponent {
           <Item inlineLabel>
             <Label>Username</Label>
             <Input
+              onChangeText={this.handleUsername}
               placeholder={'your@email.com'}
               style={styles.input}
+              value={username}
             />
           </Item>
           <Item inlineLabel>
             <Label>Password</Label>
             <Input
+              onChangeText={this.handlePassword}
               placeholder={'your-email-password'}
               secureTextEntry={true}
               style={styles.input}
+              value={password}
             />
           </Item>
           <View style={styles.buttonContainer}>
-            <Button block light>
+            <Button
+              onPress={this.onLogin}
+              block
+              light
+            >
               <Text style={styles.buttonText}>
                 Conectar
               </Text>
